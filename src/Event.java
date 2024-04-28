1163e0b975ea7a4546f2811d203278937ec000c9
Didem Sarıkaya <didemsarikaya_41@hotmail.com>
2024-04-28 22:43:45 +0300

    Event class added

---
+++ src/Event.java
@@ -0,0 +1,36 @@
+public class Event {
+    private String eventId;
+    private int eventTime;
+    private EventType eventType;
+
+
+    public Event(String eventId, int eventTime, EventType eventType) {
+        this.eventId = eventId;
+        this.eventTime = eventTime;
+        this.eventType = eventType;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public int getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(int eventTime) {
+        this.eventTime = eventTime;
+    }
+
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(EventType eventType) {
+        this.eventType = eventType;
+    }
+}
