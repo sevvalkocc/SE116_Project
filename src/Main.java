d121f94fb839c926cba64de76ecc46225b624459
Ezgiercin <ezgiiercinn@gmail.com>
2024-04-30 22:23:50 +0300

    File accessibility and existence checked.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length != 2) {
+            System.out.println("Please write in this format: java Main <workflow_file> <job_file>");
+            return;
+        }
+            String workflowFileName;
+            String jobFileName;
+
+            if(args[0].endsWith(".job")){
+                jobFileName=args[0];
+                workflowFileName=args[1];
+            }else{
+                jobFileName=args[1];
+                workflowFileName=args[0];
+            }
+            checkFile(workflowFileName);
+            checkFile(jobFileName);
     }
+        private static boolean checkFile(String fileName){
+            File file=new File(fileName);
+            if(!file.exists()){
+                System.out.println("Error: File "+ fileName +" does not exist");
+                return false;
+            }
+            if (!file.canRead()) {
+                System.out.println("Error: File " + fileName + " is not accessible.");
+                return false;
+            }
+            System.out.println("File " + fileName + " exists and is accessible.");
+            return true;
+        }
 }
