e733b805dff676551a909e611a60926c2c18e365
TalatKarasakal <talatkarasakal35@gmail.com>
2024-05-09 17:01:04 +0300

    JobType class updated.

--- src/JobType.java
+++ src/JobType.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+
 public class JobType {
+    private String jobTypeId;
+    private ArrayList<TaskType> taskTypes;;
+    private double size;
+
+    public String getJobTypeId() {
+        return jobTypeId;
+    }
+
+    public void setJobTypeId(String jobTypeId) {
+        this.jobTypeId = jobTypeId;
+    }
+
+    public JobType(String jobTypeId, ArrayList<TaskType> taskTypes, double size) {
+        this.jobTypeId = jobTypeId;
+        this.taskTypes = taskTypes;
+        this.size = size;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public ArrayList<TaskType> getTaskTypes() {
+        return taskTypes;
+    }
+
+    public void setTaskTypes(ArrayList<TaskType> taskTypes) {
+        this.taskTypes = taskTypes;
+    }
+
 }
